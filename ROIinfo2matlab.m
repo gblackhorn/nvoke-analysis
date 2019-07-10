df4955ff49e6c1cdc0b2de63974c46c4f4c2b479
gblackhorn <tabrias@gmail.com>
2019-07-10 14:31:48 +0900

    added ROIinfo2matlab.m. import multiple ROI readout excel files into a .mat file for further analysis

---
+++ ROIinfo2matlab.m
@@ -0,0 +1,37 @@
+function [ROIdata, varargout] = ROIinfo2matlab(inputArg)
+%ROIinfo2matlab 
+% read all ROI.csv in a folder and get info into matlab
+%   Detailed explanation goes here
+
+input_dir = 'G:\Workspace\Inscopix_Seagate\Projects';
+output_dir = 'G:\Workspace\Inscopix_Seagate\Analysis';
+
+roi_readout_file_folder = uigetdir(input_dir, 'Select a folder containing ROI readout files');
+
+roi_readout_file_info = dir([roi_readout_file_folder, '\', '*-ROI.csv']);
+
+cell_num = 0;
+
+for n = 1:numel(roi_readout_file_info)
+	roi_readout_file = fullfile(roi_readout_file_info(n).folder, roi_readout_file_info(n).name);
+	opts = detectImportOptions(roi_readout_file); % creat import options based on file content
+	opts.DataLine = 3; % set data line from the third row of csv file. First 2 rows are 'char'
+	opts.VariableDescriptionsLine = 2; % set 2nd row as variable description
+	ROI_table = readtable(roi_readout_file, opts); % import file using modified opts, so data will be number arrays
+	[ROI_table, recording_time, ROI_num] = ROI_calc_plot(ROI_table);
+	cell_num = cell_num + size(ROI_table, 2);
+	ROIdata{n, 1} = roi_readout_file_info(n).name;
+	ROIdata{n, 2} = ROI_table;
+end
+varargout{1} = numel(roi_readout_file_info); % recording numbers
+varargout{2} = cell_num; % total cell numbers
+[ROIdata_file, ROIdata_path] = uiputfile([output_dir, '/*.mat'], 'Save ROIdata into a .matfile');
+if isequal(ROIdata_file,0) || isequal(ROIdata_path,0)
+   disp('User clicked Cancel.')
+else
+   disp(['User selected ',fullfile(ROIdata_path,ROIdata_file),...
+         ' and then clicked Save.'])
+   save(fullfile(ROIdata_path,ROIdata_file),'ROIdata');
+end
+
+
