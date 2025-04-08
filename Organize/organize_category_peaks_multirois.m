6eed5b5372cf0dcc850e8c46a736405c833c6222
gblackhorn <guoda.gd@gmail.com>
2025-04-08 18:01:48 +0900

    Optimize the comments in organize_category_peaks_multirois

--- Organize/organize_category_peaks_multirois.m
+++ Organize/organize_category_peaks_multirois.m
@@ -1,21 +1,32 @@
 function [peak_properties_tables_with_cat,varargout] = organize_category_peaks_multirois(peak_properties_tables,gpio_info_table,varargin)
-    % Return peak_category for the whole recording (multiple rois)
-    % Caution (2021.01.10): only works with up to 2 stimulation channels so far 
-    %   peak_properties_tables: multiple roi table
-    %   gpio_info_table: output of function "organize_gpio_info". multiple stim_ch can be used
+    % ORGANIZE_CATEGORY_PEAKS_MULTIROIS Categorize peaks for multiple ROIs based on stimulation information.
+    %   [peak_properties_tables_with_cat, ...] = ORGANIZE_CATEGORY_PEAKS_MULTIROIS(peak_properties_tables, gpio_info_table)
+    %   returns a cell array of tables with categorized peaks for multiple ROIs.
+    %
+    %   Inputs:
+    %       peak_properties_tables - Cell array of tables containing peak properties for multiple ROIs.
+    %       gpio_info_table - Table containing GPIO information (output of function "organize_gpio_info").
+    %
+    %   Optional Inputs:
+    %       'eventTimeType' - Time type used to categorize events (default: 'peak_time').
+    %       'stim_time_error' - Time error to account for low temporal resolution (default: 0).
+    %       'criteria_excitated' - Time window for triggered peaks (default: 2 seconds).
+    %       'criteria_rebound' - Time window for rebound peaks (default: 1 second).
+    %
+    %   Outputs:
+    %       peak_properties_tables_with_cat - Cell array of tables with categorized peaks for multiple ROIs.
+    %
+    %   Example:
+    %       [peak_properties_tables_with_cat] = organize_category_peaks_multirois(peak_properties_tables, gpio_info_table, ...
+    %           'eventTimeType', 'peak_time', 'stim_time_error', 0, 'criteria_excitated', 2, 'criteria_rebound', 1);
 
-    % event_category_names
-    % {'noStim', 'beforeStim', 'interval', 'trigger', 'delay', 'rebound'};
-    
     % Defaults
     eventTimeType = 'peak_time'; % peak_time/rise_time. Use this value to categorize event
-    stim_time_error = 0; % due to low temperal resolution and error in lowpassed data, start and end time point of stimuli can be extended
+    stim_time_error = 0; % due to low temporal resolution and error in lowpassed data, start and end time point of stimuli can be extended
     criteria_excitated = 2; % triggered peak: peak start to rise in 2s from onset of stim
     criteria_rebound = 1; % rebound peak: peak start to rise in 1s from end of stim
-    % peak_cat_str = {'noStim', 'noStimFar', 'triggered', 'triggered_delay', 'rebound', 'interval'};
-    % [peak_cat_str] = event_category_names;
 
-    % Optionals
+    % Parse optional inputs
     for ii = 1:2:(nargin-2)
     	if strcmpi('eventTimeType', varargin{ii})
     		eventTimeType = varargin{ii+1};
@@ -28,8 +39,7 @@ function [peak_properties_tables_with_cat,varargout] = organize_category_peaks_m
     	end
     end
 
-    % main contents
-    % peak_properties_tables_with_cat = peak_properties_tables;
+    % Initialize output
     peak_properties_tables_with_cat = cell(size(peak_properties_tables));
 
     roi_num = size(peak_properties_tables, 2);
@@ -45,9 +55,9 @@ function [peak_properties_tables_with_cat,varargout] = organize_category_peaks_m
         	if ~isempty(gpio_info_table)
     	    	peak_category = cell(length(peak_properties_table_single.rise_time), stim_ch_num);
     	    	for sn = 1:stim_ch_num
-    	    		[peak_category(:, sn)] = organize_category_peaks(peak_properties_table_single,...
-    	    			gpio_info_table(sn, :),'eventTimeType',eventTimeType, 'stim_time_error', stim_time_error,...
-                        'criteria_excitated',criteria_excitated,'criteria_rebound',criteria_rebound);
+    	    		peak_category(:, sn) = organize_category_peaks(peak_properties_table_single, ...
+    	    			gpio_info_table(sn, :), 'eventTimeType', eventTimeType, 'stim_time_error', stim_time_error, ...
+                        'criteria_excitated', criteria_excitated, 'criteria_rebound', criteria_rebound);
     	    	end
     	    	if stim_ch_num == 2
                     if strcmp(peak_category(:, 1), peak_category(:, 2))
@@ -57,14 +67,14 @@ function [peak_properties_tables_with_cat,varargout] = organize_category_peaks_m
                     end
     	    	end
     	    else
-    	    	[peak_category] = organize_category_peaks(peak_properties_table_single,...
-    	    		gpio_info_table, 'eventTimeType',eventTimeType, 'stim_time_error', stim_time_error);
+    	    	peak_category = organize_category_peaks(peak_properties_table_single, ...
+    	    		gpio_info_table, 'eventTimeType', eventTimeType, 'stim_time_error', stim_time_error);
     	    end
-            peak_properties_table_single = addvars(peak_properties_table_single,peak_category);
+            peak_properties_table_single = addvars(peak_properties_table_single, peak_category);
         end
         peak_properties_tables_with_cat{1, rn} = peak_properties_table_single;
     end
-    peak_properties_tables_with_cat = cell2table(peak_properties_tables_with_cat,...
+    peak_properties_tables_with_cat = cell2table(peak_properties_tables_with_cat, ...
         'VariableNames', peak_properties_tables.Properties.VariableNames);
 end
 
